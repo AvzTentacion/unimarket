e821c2477e868e008931799d5a50222e2b5db94f
AvzTentacion <aviwemhlatyana249@gmail.com>
2025-07-30 20:13:47 +0200

    i changed the instagram link

--- src/components/TrendingItems.jsx
+++ src/components/TrendingItems.jsx
@@ -45,7 +45,7 @@ const TrendingItems = () => {
                 </a>
             </div>
 
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-6">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
                 {dummyTrendingItems.map((item) => (
                     <ItemCard key={item.id} item={item} />
                 ))}
